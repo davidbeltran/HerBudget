9e37beea0b98a5ccc901eaf9b0ac68fea70fee2b
David Beltran <beltrannowd5@gmail.com>
2024-07-16 15:35:17 -0700

    started expense class

--- HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
+++ HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed555b79a15452911d91152c52b3916293af980d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa2742af540a940d231d299c486e553a95f90e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
