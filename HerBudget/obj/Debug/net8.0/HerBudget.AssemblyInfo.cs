b3b62034a6a8336fbcd683734e8cb959655ec961
David Beltran <beltrannowd5@gmail.com>
2024-08-28 14:53:20 -0700

    firsts

--- HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
+++ HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131d4660492b75b8c89e61a6b36c9907260c5425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674e0740a6d89236841ee7ffafe27c3c26026148")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
