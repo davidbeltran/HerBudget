73f9724745b8971a43e421a169f36a9df86e931b
David Beltran <beltrannowd5@gmail.com>
2024-09-03 15:12:10 -0700

    added leetcode link

--- HerBudget/Spreadsheet.cs
+++ HerBudget/Spreadsheet.cs
@@ -44,7 +44,7 @@ namespace HerBudget
             workbook.Save();
             workbook.Close();
         }
-
+        // https://leetcode.com/problemset/
         private void AddBills(Excel._Worksheet sheet)
         {
             double Internet = 0, Car_Insurance = 0, Housing = 0, Energy = 0, Gas = 0, Income = 0,
