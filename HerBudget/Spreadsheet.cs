0cdcae982545ae4319fa4af8a4e8025a8c6a585e
David Beltran <beltrannowd5@gmail.com>
2024-09-26 12:58:35 -0700

    finding specific worksheet

--- HerBudget/Spreadsheet.cs
+++ HerBudget/Spreadsheet.cs
@@ -48,7 +48,8 @@ namespace HerBudget
             {
                 Excel.Workbook workbook = excel.Workbooks.Open(fullPath);
                 Excel.Sheets worksheets = workbook.Sheets;
-
+                //workbook.Worksheets[firstExp.Month];
+                // https://reference.aspose.com/tutorials/cells/net/excel-worksheet-csharp-tutorials/get-excel-worksheet-by-name-csharp-tutorial/
                 Excel._Worksheet worksheet = (Excel.Worksheet)worksheets.Add(worksheets[1],
                     System.Type.Missing, System.Type.Missing, System.Type.Missing);
                 worksheet.Name = "prueba";
--- HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
+++ HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f51066c84b5acc103891f1beb8898a281535692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6e68a676b654071ba4e55af6c7eff337e8f417")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
