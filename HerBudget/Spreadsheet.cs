f5081bc5be074dae8cae8f26f3b6ee34db7a2402
David Beltran <beltrannowd5@gmail.com>
2024-10-03 13:36:18 -0700

    possible fix for sheets.count

--- HerBudget/Spreadsheet.cs
+++ HerBudget/Spreadsheet.cs
@@ -74,7 +74,7 @@ namespace HerBudget
             }
             else
             {
-                Excel._Worksheet worksheet = (Excel.Worksheet)sheets.Add(sheets[sheets.Count + 1],
+                Excel._Worksheet worksheet = (Excel.Worksheet)sheets.Add(sheets[sheets.Count],
                     System.Type.Missing, System.Type.Missing, System.Type.Missing);
                 worksheet.Name = firstExp.Month;
                 MakeHeaders(worksheet);
