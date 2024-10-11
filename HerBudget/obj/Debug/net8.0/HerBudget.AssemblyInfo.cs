bbecd84ba9ba0c320558c942c28647637a036a2f
David Beltran <beltrannowd5@gmail.com>
2024-10-11 13:19:58 -0700

    does not look like excel.range is null

--- HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
+++ HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4cea0fcfe233a16c3ee7d348be57826f5556f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425cab1d8ce9d13fd4948b7ffa6dafdd4f35450e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
