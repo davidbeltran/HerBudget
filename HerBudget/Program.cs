1731796bf1c3a82796f5f43d7af7e4a0b3fbf464
David Beltran <beltrannowd5@gmail.com>
2024-10-21 14:25:33 -0700

    corrected mistake

--- HerBudget/Program.cs
+++ HerBudget/Program.cs
@@ -9,6 +9,7 @@ namespace HerBudget
         static void Main(string[] args)
         {
             PathCreator pc = new PathCreator("HerBudget\\pdfs", "MarApr24C.pdf");
+            string pathPdf = pc.MakeFile();
             //string pathPdf = "D:/afterGrad/c#/Adelisa/HerBudget/pdfs/MarApr24C.pdf";
 
             Statement stmt = new Statement(pathPdf);
