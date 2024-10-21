dec99877ab68f36bdf64423700bf3e26610428c0
David Beltran <beltrannowd5@gmail.com>
2024-10-21 13:04:09 -0700

    created attributes and constructor

--- HerBudget/PathCreator.cs
+++ HerBudget/PathCreator.cs
@@ -2,5 +2,13 @@
 {
     public class PathCreator
     {
+        public string NewDirectory { get; set; }
+        public string NewFile { get; set; }
+
+        public PathCreator(string NewDirectory, string NewFile)
+        {
+            this.NewDirectory = NewDirectory;
+            this.NewFile = NewFile;
+        }
     }
 }
