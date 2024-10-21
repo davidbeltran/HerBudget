5eb37c8880d306cbdabdf1187f78d2f76137995a
David Beltran <beltrannowd5@gmail.com>
2024-10-21 13:54:08 -0700

    improving PathCreator

--- HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
+++ HerBudget/obj/Debug/net8.0/HerBudget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb382cf35737913c0d065868850ff06f9cbcdff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068c3f009a99edcda32116b0ea03cdf3fa489304")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerBudget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
