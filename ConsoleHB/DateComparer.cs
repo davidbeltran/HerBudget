caefcf7a72dbba55034429c34858ca07f158d482
David Beltran <beltrannowd5@gmail.com>
2024-10-28 15:12:57 -0700

    still searching for fix

--- ConsoleHB/DateComparer.cs
+++ ConsoleHB/DateComparer.cs
@@ -13,8 +13,8 @@ namespace ConsoleHB
     {
         public int Compare(object? x, object? y)
         {
-            Expense? exp1 = x as Expense;
-            Expense? exp2 = y as Expense;
+            Expense exp1 = x as Expense;
+            Expense exp2 = y as Expense;
             return exp1!.Month.CompareTo(exp2!.Month);
         }
     }
