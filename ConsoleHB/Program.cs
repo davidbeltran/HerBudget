46fb248080b89d3cc432e9d6183427608760e45d
David Beltran <beltrannowd5@gmail.com>
2024-11-11 08:32:51 -0800

    firsts

--- ConsoleHB/Program.cs
+++ ConsoleHB/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleHB
     {
         static void Main(string[] args)
         {
-            PathCreator pc = new PathCreator("pdfs", "NovDec23C.pdf");
+            PathCreator pc = new PathCreator("pdfs", "SepOct24A.pdf");
             string pathPdf = pc.MakeFile();
 
             Statement stmt = new Statement(pathPdf);
